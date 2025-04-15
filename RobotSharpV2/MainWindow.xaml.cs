8e87ead78ec21f93bd01c7b61522216a17fe529f
Horizon008 <116902866+Horizon008@users.noreply.github.com>
2025-04-15 14:06:25 +0300

    Коррекция тона

--- RobotSharpV2/MainWindow.xaml.cs
+++ RobotSharpV2/MainWindow.xaml.cs
@@ -15,6 +15,7 @@ namespace RobotSharpV2
         private VideoCapture _capture;
         private bool _isCapturing;
         private Mat _currentFrame = new Mat();
+        private int _binarizationLevel = 100; 
 
         [DllImport("gdi32.dll")]
         private static extern bool DeleteObject(IntPtr handle);
@@ -77,14 +78,33 @@ namespace RobotSharpV2
         {
             var outputFrame = inputFrame.Clone();
             maskFrame = new Mat();
-            fingerCount = 0; 
+            fingerCount = 0;
 
-            using (Mat grayFrame = new Mat())
-            using (Mat thresholdFrame = new Mat())
+            
+            Mat grayFrame = new Mat();
+            Mat thresholdFrame = new Mat();
+            Mat blurredFrame = new Mat();
+            Mat medianFilteredFrame = new Mat();
+            Mat dilatedFrame = new Mat();
+            Mat erodedFrame = new Mat();
+
+            try
             {
+                
                 CvInvoke.CvtColor(inputFrame, grayFrame, ColorConversion.Bgr2Gray);
-                CvInvoke.Threshold(grayFrame, thresholdFrame, 100, 255, ThresholdType.Binary);
 
+                
+                CvInvoke.Threshold(grayFrame, thresholdFrame, _binarizationLevel, 255, ThresholdType.Binary);
+
+                
+                CvInvoke.MedianBlur(thresholdFrame, medianFilteredFrame, 5);
+                thresholdFrame = medianFilteredFrame.Clone();
+                var kernel = CvInvoke.GetStructuringElement(ElementShape.Rectangle, new System.Drawing.Size(3, 3), new System.Drawing.Point(-1, -1));
+                CvInvoke.Dilate(thresholdFrame, dilatedFrame, kernel, new System.Drawing.Point(-1, -1), 1, BorderType.Constant, new MCvScalar(1));
+                CvInvoke.Erode(dilatedFrame, erodedFrame, kernel, new System.Drawing.Point(-1, -1), 1, BorderType.Constant, new MCvScalar(1));
+                thresholdFrame = erodedFrame.Clone(); 
+
+                
                 maskFrame = thresholdFrame.Clone();
 
                 using (VectorOfVectorOfPoint contours = new VectorOfVectorOfPoint())
@@ -97,16 +117,18 @@ namespace RobotSharpV2
                         RetrType.External,
                         ChainApproxMethod.ChainApproxSimple);
 
+           
                     for (int i = 0; i < contours.Size; i++)
                     {
                         var contour = contours[i];
 
-                        if (CvInvoke.ContourArea(contour) > 500) 
+                        if (CvInvoke.ContourArea(contour) > 500)
                         {
-
+                            
                             var hull = new VectorOfPoint();
                             CvInvoke.ConvexHull(contour, hull, false);
 
+                            
                             fingerCount += CountFingersUsingAngles(hull);
                         }
                     }
@@ -122,6 +144,16 @@ namespace RobotSharpV2
                     }
                 }
             }
+            finally
+            {
+                
+                grayFrame.Dispose();
+                blurredFrame.Dispose();
+                medianFilteredFrame.Dispose();
+                dilatedFrame.Dispose();
+                erodedFrame.Dispose();
+            }
+
             return outputFrame;
         }
 
@@ -132,8 +164,11 @@ namespace RobotSharpV2
             var hull = new VectorOfPoint();
             CvInvoke.ConvexHull(contour, hull, false);
 
-
             System.Drawing.Point[] hullPoints = hull.ToArray();
+
+            
+            if (hullPoints.Length < 5)
+                return fingerCount;
 
             System.Windows.Point[] wpfHullPoints = new System.Windows.Point[hullPoints.Length];
             for (int i = 0; i < hullPoints.Length; i++)
@@ -141,27 +176,58 @@ namespace RobotSharpV2
                 wpfHullPoints[i] = new System.Windows.Point(hullPoints[i].X, hullPoints[i].Y);
             }
 
+           
             for (int i = 0; i < wpfHullPoints.Length; i++)
             {
                 System.Windows.Point pt1 = wpfHullPoints[i];
-                System.Windows.Point pt2 = wpfHullPoints[(i + 1) % wpfHullPoints.Length]; 
-                System.Windows.Point pt3 = wpfHullPoints[(i + 2) % wpfHullPoints.Length]; 
+                System.Windows.Point pt2 = wpfHullPoints[(i + 1) % wpfHullPoints.Length];
+                System.Windows.Point pt3 = wpfHullPoints[(i + 2) % wpfHullPoints.Length];
 
                 double angle = GetAngle(pt1, pt2, pt3);
 
                 
-                if (angle < 45) 
+                if (angle > 45) 
                 {
-                    fingerCount++;
+                   
+                    System.Windows.Point pt4 = wpfHullPoints[(i + 3) % wpfHullPoints.Length];
+
+                    double angleAfter = GetAngle(pt2, pt3, pt4);
+
+                   
+                    if (Math.Abs(angleAfter - 180) < 20)
+                    {
+                        
+                        double distance = GetDistance(pt1, pt3);
+
+         
+                        if (distance < 50) 
+                        {
+                            fingerCount++;
+                        }
+                    }
                 }
+            }
+
+           
+            double contourPerimeter = CvInvoke.ArcLength(contour, true);
+            double contourArea = CvInvoke.ContourArea(contour);
+
+         
+            if (contourArea > 500 && contourPerimeter / contourArea > 5)
+            {
+                
+                fingerCount++;
             }
 
             return fingerCount;
         }
 
+        private double GetDistance(System.Windows.Point pt1, System.Windows.Point pt2)
+        {
+            return Math.Sqrt(Math.Pow(pt2.X - pt1.X, 2) + Math.Pow(pt2.Y - pt1.Y, 2));
+        }
 
-
-        private double GetAngle(Point pt1, Point pt2, Point pt3)
+        private double GetAngle(System.Windows.Point pt1, System.Windows.Point pt2, System.Windows.Point pt3)
         {
             double angle = Math.Abs(Math.Atan2(pt3.Y - pt2.Y, pt3.X - pt2.X) - Math.Atan2(pt1.Y - pt2.Y, pt1.X - pt2.X));
             if (angle > Math.PI)
@@ -199,6 +265,12 @@ namespace RobotSharpV2
                     DeleteObject(hBitmap);
                 }
             }
+        }
+
+      
+        private void BinarizationSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            _binarizationLevel = (int)e.NewValue;
         }
 
         private void StopCapture()
